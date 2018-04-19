cc3a5df75ae840e4a61fe116f612685b2cf09904
lobna <lobnazakaria63@yahoo.com>
2018-04-19 11:49:15 +0200

    adding images in assets folder, adding a text in the footer with decreasing line-height, modifying styling navbar with arabic translate in scroll=0 and above 3 and also with mobile

--- CapitalPaints/src/app/app.component.ts
+++ CapitalPaints/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import * as $ from 'jquery';
 @Component({
@@ -6,17 +6,42 @@ import * as $ from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   toAr:boolean=false;
   toEn:boolean=true;
+  
   constructor(private translate:TranslateService) {
     translate.setDefaultLang('en');
    }
 
+   ngOnInit(){
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      
+      if (scroll==0){
+        $('#nav-style').removeClass('navbar-default');
+        $('#nav-style').addClass('navStyle');
+        $('#logo-nav').removeClass('logo-img');
+        $('#logo-nav').addClass('logoStyle');
+        $('.navLink').removeClass('nav-link');
+        $('.navLink').addClass('navlinkStyle');
+        
+      }
+      if(scroll>3){
+        $('#nav-style').removeClass('navStyle');
+        $('#nav-style').addClass('navbar-default');
+        $('#logo-nav').removeClass('logoStyle');
+        $('#logo-nav').addClass('logo-img');
+        $('.navLink').removeClass('navlinkStyle');
+        $('.navLink').addClass('nav-link');
+      }
+  });
+   }
    switchLanguageAr(language:string){
     this.translate.use(language);
     this.toggleBtnAr();
+    
   }
   toggleBtnAr(){
     this.toAr=true;
@@ -30,4 +55,7 @@ export class AppComponent {
     this.translate.use(language);
     this.toggleBtnEn();
   }
+ 
+
+
 }
